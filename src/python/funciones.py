4d098806828b948015f1f85ac84a1b36a88e2193
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-04-11 08:28:56 -0300

    arreglos fuerza bruta y backtracking

--- src/python/funciones.py
+++ src/python/funciones.py
@@ -22,26 +22,54 @@ def error_segmento(bp1:List[int], bp2:List[int], grilla_x, grilla_y, valores_x, 
     return error_aux
 
 def fuerza_bruta(B:List[List[int]], k:int, m1:int, m2:int, grilla_x, grilla_y, valores_x, valores_y, minimo:List[List[int]], error_total, error_minimo):
-    error_segmento1 = 0
-    if len(B) == k:
-        if error_total < error_minimo and B[-1][0] == m1-1:
-            error_minimo = error_total
-            minimo[:] = B[:]
-        return minimo, error_minimo
+        error_segmento1 = 0
+        if len(B) == k:
+            if error_total < error_minimo and B[-1][0] == m1-1:
+                error_minimo = error_total
+                minimo[:] = B[:]
+            return minimo, error_minimo
 
-    else:
-        for i in range(m2):
+        else:
             if len(B) == 0:
-                B.append([0, i])
-
-            for l in range(int(B[-1][0]) + 1, m1):
-                for j in range(m2):
-                    B.append([l, j])
-                    error_segmento1=error_segmento([l-1, j-1], [l, j], grilla_x, grilla_y, valores_x, valores_y)
-                    error_total+=error_segmento1
+                for i in range(m2):
+                    B.append([0, i])
                     minimo, error_minimo = fuerza_bruta(B, k, m1, m2, grilla_x, grilla_y, valores_x, valores_y, minimo, error_total, error_minimo)
                     B.pop()
-                    error_total -= error_segmento1
-            if len(B) == 1:
-                B.pop() #se queda loopeado, no saca el primer elemento que agregamos
-        return minimo, error_minimo
+            else:
+                for l in range(int(B[-1][0]) + 1, m1):
+                    for j in range(m2):
+                        B.append([l, j])
+                        error_segmento1=error_segmento([l-1, j-1], [l, j], grilla_x, grilla_y, valores_x, valores_y)
+                        error_total+=error_segmento1
+                        minimo, error_minimo = fuerza_bruta(B, k, m1, m2, grilla_x, grilla_y, valores_x, valores_y, minimo, error_total, error_minimo)
+                        B.pop()
+                        error_total -= error_segmento1
+
+            return minimo, error_minimo
+        
+
+def backtracking(B:List[List[int]], k:int, m1:int, m2:int, grilla_x, grilla_y, valores_x, valores_y, minimo:List[List[int]], error_total, error_minimo):
+        error_segmento1 = 0
+        if len(B) == k:
+            if error_total < error_minimo and B[-1][0] == m1-1:
+                error_minimo = error_total
+                minimo[:] = B[:]
+            return minimo, error_minimo
+
+        elif error_total < error_minimo:
+            if len(B) == 0:
+                for i in range(m2):
+                    B.append([0, i])
+                    minimo, error_minimo = backtracking(B, k, m1, m2, grilla_x, grilla_y, valores_x, valores_y, minimo, error_total, error_minimo)
+                    B.pop()
+            else:
+                for l in range(int(B[-1][0]) + 1, m1):
+                    for j in range(m2):
+                        B.append([l, j])
+                        error_segmento1=error_segmento([l-1, j-1], [l, j], grilla_x, grilla_y, valores_x, valores_y)
+                        error_total+=error_segmento1
+                        minimo, error_minimo = backtracking(B, k, m1, m2, grilla_x, grilla_y, valores_x, valores_y, minimo, error_total, error_minimo)
+                        B.pop()
+                        error_total -= error_segmento1
+
+            return minimo, error_minimo
