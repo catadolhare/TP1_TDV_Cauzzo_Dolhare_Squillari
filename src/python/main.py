8ab7d197233f6f1a33455262d45ec27db97515a3
CatalinaDolhare <cdolhare@udesa.edu.ar>
2024-04-16 18:11:26 -0300

    graficos

--- src/python/main.py
+++ src/python/main.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import funciones
 import time
+import matplotlib.pyplot as plt
 
 BIG_NUMBER = 1e10 # Revisar si es necesario.
 
@@ -26,14 +27,13 @@ def main():
 
 	# TODO: aca se deberia ejecutar el algoritmo.
 
-	breakpoints = []
 	inicio = time.time()
-	minimom, error_minimom = funciones.llamada_backtracking(breakpoints, K, m1, m2, grid_x, grid_y, instance["x"], instance["y"])
+	minimo_main, error_minimo_main = funciones.llamada_backtracking([], K, m1, m2, grid_x, grid_y, instance["x"], instance["y"])
 	fin = time.time()
 	tiempo = fin - inicio
 	print(tiempo)
-	print(minimom)
-	print(error_minimom)
+	print(minimo_main)
+	print(error_minimo_main)
 
 	best = {}
 	best['sol'] = [None]*(N+1)
@@ -44,8 +44,8 @@ def main():
 	# La solucion es una lista de tuplas (i,j), donde:
 	# - i indica el indice del punto de la discretizacion de la abscisa
 	# - j indica el indice del punto de la discretizacion de la ordenada.
-	best['sol'] = breakpoints
-	best['obj'] = error_minimom
+	best['sol'] = minimo_main
+	best['obj'] = error_minimo_main
 	best['tiempo'] = tiempo
 
 	# Represetnamos la solucion con un diccionario que indica:
@@ -58,6 +58,19 @@ def main():
 	solution['y'] = [grid_y[x[1]] for x in best['sol']]
 	solution['obj'] = best['obj']
 	solution['tiempo'] = best['tiempo']
+
+	print(solution['x'])
+	print(solution['y'])
+
+	plt.plot(instance['x'], instance['y'], 'k.')
+	plt.plot(solution['x'], solution['y'], 'r-')
+	titulo = "Solución para " + instance_name + " con " + str(K) + " breakpoints"
+	plt.title(titulo)
+	plt.legend(["Datos", "Aproximación PWL"])
+	plt.grid(True)
+	plt.xticks(grid_x)
+	plt.yticks(grid_y)
+	plt.show()
 
 	# Se guarda el archivo en formato JSON
 	with open('solution_backtracking_' + str(m1) +'_' + str(m2) + '_' + str(K) + '_' + instance_name, 'w') as f:
